56dce2a611ba60365ebd577e6ca188f2ea66386b
= <=>
2025-06-24 05:53:47 +0000

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""This module defines a class Square."""
+
 class Square:
+    """Represents a square with a private size attribute."""
+    
     def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square (no validation).
+        """
         self.__size = size
